0c47c203d87e575dbd1b6324473ddce9b536758a
liamswearengin@gmail.com <liamswearengin@gmail.com>
2025-03-04 20:36:49 +0000

    whoops

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import requests
 import passwords
-response = requests.get('http://localhost:8080/basic-auth/Liam/test',auth=(passwords.USERNAME, passwords.PASSWORD))
+response = requests.get('http://localhost:80/basic-auth/Liam/test',auth=(passwords.USERNAME, passwords.PASSWORD))
 print(response.status_code)
